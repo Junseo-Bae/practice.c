86a82aedfd6b39383018211f2a76be59a0d1f8c8
Junseo-Bae <74636948+Junseo-Bae@users.noreply.github.com>
2025-04-11 18:55:36 +0900

    Update 배열 뒤집기, 정렬 1.c

--- 배열 뒤집기, 정렬 1.c
+++ 배열 뒤집기, 정렬 1.c
@@ -12,7 +12,7 @@ int main()
     for(i = 0; i < 5 / 2; i++)
     {
         temp = arr[i];
-        arr[i] = arr[5 - 1- i];
+        arr[i] = arr[5 - 1 - i];
         arr[5 - 1 - i] = temp;
     }
 
