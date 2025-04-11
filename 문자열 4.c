16f19dc9f9c8c7b7824d0aca0c0d14d7a66b3624
Junseo-Bae <74636948+Junseo-Bae@users.noreply.github.com>
2025-04-11 17:45:53 +0900

    Create 문자열 4.c

---
+++ 문자열 4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// 짝수만 출력
+
+int main()
+{
+    char str[10];
+    int i;
+
+    scanf("%s", str);
+
+    for(i = 0; i < strlen(str); i++)
+    {
+        if(str[i] % 2 == 0)
+        {
+            printf("%c ", str[i]);
+        }
+    }
+
+    return 0;
+}
