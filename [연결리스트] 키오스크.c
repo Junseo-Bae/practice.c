e915b054163fbaf3e705270d7b6598f63834879e
Junseo-Bae <74636948+Junseo-Bae@users.noreply.github.com>
2025-05-03 17:35:14 +0900

    Update [연결리스트] 키오스크.c

--- [연결리스트] 키오스크.c
+++ [연결리스트] 키오스크.c
@@ -14,41 +14,49 @@ struct LinkedList {
 int main()
 {
     int i;
-    
+
     struct LinkedList ll; //16 byte
     struct Node* temp;
-    
+
     ll.first = null;
     ll.last = null;
-    
+
     // first order
     temp = (struct Node*)malloc(sizeof(struct Node)*1)
     temp->order = 1; //(*temp).order
     temp->next = null;
-        
+
     ll.first = temp;
     ll.last = temp;
-    
+
     // second order
     temp = (struct Node*)malloc(sizeof(struct Node)*1)
     temp->order = 2;
     temp->next = null;
-    
+
     ll.last = temp;
     ll.first->next = temp;
-    
+
     // third order
     temp = (struct Node*)malloc(sizeof(struct Node)*1)
     temp->order = 3;
     temp->next = null;
-    
+
     ll.last->next = temp;
     ll.last = temp;
-    
+
     // del first
     temp = ll.first;
     ll.first = ll.first->next;
     free(temp);
+
+    // print order
+    temp = ll.first;
+    while(temp != null)
+    {
+        printf("%d >> ", temp->order);
+        temp = temp->next;
+    }
 
     return 0;
 }
