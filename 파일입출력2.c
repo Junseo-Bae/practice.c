fc82dc39e2b3ba9f9b26f706c68fefd42cc292e4
Junseo-Bae <74636948+Junseo-Bae@users.noreply.github.com>
2025-05-17 17:22:21 +0900

    Create 파일입출력2.c

---
+++ 파일입출력2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    FILE* fp = fopen("text.txt", "w");;
+    char str[100];
+    int i;
+
+    for(i = 0; i < 3; i++)
+    {
+        scanf("%s", str);
+        fputs(str, fp);
+        fputs("\n", fp);
+    }
+    fclose(fp);
+
+    fp = fopen("text.txt", "r");
+    for(i = 0; i < 3; i++)
+    {
+        fgets(str, 100, fp);
+        printf("%s", str);
+    }
+    fclose(fp);
+
+	return 0;
+}
