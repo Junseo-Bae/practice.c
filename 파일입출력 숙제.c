a00ed14817636f7ca78e7872a5e9bed82fc45dd3
Junseo-Bae <74636948+Junseo-Bae@users.noreply.github.com>
2025-05-23 17:48:52 +0900

    Update 파일입출력 숙제.c

--- 파일입출력 숙제.c
+++ 파일입출력 숙제.c
@@ -1,21 +1,34 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 
 int main()
 {
     FILE* file;
     
     // 파일 쓰기 텍스트 모드로 열기
+    file = fopen("test.txt", "w");
     
     // 파일에 a문자를 랜덤 n개 만큼 쓰기
+    srand(time(NULL));
+    int i, n;
     
+    n = rand() & 10 + 1;
+    
+    for(i = 0; i < n; i++)
+    {
+        fputc('a', file);
+    }
     // 파일 닫기
+    fclose(file);
     
-    // 파잉을 읽기 모드로 열기
+    // 파일을 읽기 모드로 열기
+    file = fopen("test.txt", "r");
     
     // 파일 사이즈(byte)를 구해서 출려하기
     
     // 파일 닫기
+    fclose(file);
     
 	return 0;
 }
