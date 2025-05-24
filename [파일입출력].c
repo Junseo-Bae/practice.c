ba4fbf5169dd7a67b3619f8a5931ebec15e15e5e
Junseo-Bae <74636948+Junseo-Bae@users.noreply.github.com>
2025-05-24 16:44:15 +0900

    Create [파일입출력].c

---
+++ [파일입출력].c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+struct student {
+    char name[10];
+    int std_num;
+    int score[5];
+    float avg;
+};
+
+int main()
+{
+    struct student std[100];
+    int i, j, n;
+    srand(time(NULL));
+
+    for(i = 0; i < 100; i++)
+    {
+        n = rand() % 7 + 3;
+        for(j = 0; j < n; j++)
+        {
+            std[i].name[j] = rand() % 26 + 'a';
+        }
+        std[i].name[j] = '\0';
+
+        std[i].std_num = (rand() % 200 + 1) + 2025000;
+
+        for(j = 0; j < 5; j++)
+        {
+            std[i].score[j] = rand() % 100 + 1;
+        }
+
+        std[i].avg = 0.0f;
+    }
+
+    FILE* file;
+
+    file = fopen("student.bin", "wb");
+    fwrite(std, sizeof(struct student), 100, file);
+    fclose(file);
+    
+	return 0;
+}
