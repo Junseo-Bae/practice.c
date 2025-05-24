6ac6b2bb6b64c010dbb0e161e96187e63356e3ca
Junseo-Bae <74636948+Junseo-Bae@users.noreply.github.com>
2025-05-24 17:16:07 +0900

    Update 파일입출력 숙제.c

--- 파일입출력 숙제.c
+++ 파일입출력 숙제.c
@@ -5,37 +5,32 @@
 int main()
 {
     FILE* file;
-    
+
     // 파일 쓰기 텍스트 모드로 열기
     file = fopen("test.txt", "w");
-    
+
     // 파일에 a문자를 랜덤 n개 만큼 쓰기
     srand(time(NULL));
     int i, n;
-    
-    n = rand() & 10 + 1;
-    
+
+    n = rand() % 10 + 1;
+
     for(i = 0; i < n; i++)
     {
-        fputc('a', file);
+        fprintf(file, "%c", 'a');
     }
     // 파일 닫기
     fclose(file);
-    
+
     // 파일을 읽기 모드로 열기
     file = fopen("test.txt", "r");
-    
+
     // 파일 사이즈(byte)를 구해서 출려하기
-    
+    fseek(file, 0, SEEK_END);
+    printf("%d", ftell(file));
+
     // 파일 닫기
     fclose(file);
-    
+
 	return 0;
 }
-
-
-int fseek(FILE*, long offset, int whence);
-// whence 기준점
-// SEEK_SET, SEEK_CUR, SEEK_END
-
-long ftell(FILE*);
