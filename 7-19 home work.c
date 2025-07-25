94d17678bb46a805ae4daa109fd68ee9770ef235
Junseo-Bae <74636948+Junseo-Bae@users.noreply.github.com>
2025-07-26 08:25:40 +0900

    Update 7-19 home work.c

--- 7-19 home work.c
+++ 7-19 home work.c
@@ -1,6 +1,68 @@
-문자열을 입력받고 xor 연산을 이용해 암호화, 복호화 과정을 화면에 출력
+#include <stdio.h>
 
-  문자열을 입력 :
-  original : 
+int main()
+{
+    char str[100];
+    char enc[100];
+    char dec[100];
+    char key = bae;
 
-  encrypt :
+    printf("문자열을 입력하세요..");
+    scanf("%s", str);
+  
+    printf("original : %s\n", str);
+    for(int i = 0; str[i] != '\0'; i++)
+    {
+        unsigned char a = 1 << 7;
+        for(int j = 1; j <= 8; j++)
+        {
+            printf("%d", str[i] & a ? 1 : 0);
+            if(j == 4) printf(" ");
+            a >>= 1;
+        }
+        printf(" | ");
+    }
+    printf("\n");
+
+    for(int i = 0; str[i] != '\0'; i++)
+    {
+        enc[i] = str[i] ^ key;
+    }
+    enc[strlen(str)] = '\0';
+
+    printf("encrypt : %s\n", enc);
+    for(int i = 0; enc[i] != '\0'; i++)
+    {
+        unsigned char a = 1 << 7;
+        for(int j = 1; j <= 8; j++)
+        {
+            printf("%d", enc[i] & a ? 1 : 0);
+            if(j == 4) printf(" ");
+            a >>= 1;
+        }
+        printf(" | ");
+    }
+    printf("\n");
+
+    for(int i = 0; enc[i] != '\0'; i++)
+    {
+        dec[i] = enc[i] ^ key;
+    }
+    dec[strlen(str)] = '\0';
+
+    printf("decrypt : %s\n", dec);
+    for(int i = 0; dec[i] != '\0'; i++)
+    {
+        unsigned char a = 1 << 7;
+        for(int j = 1; j <= 8; j++)
+        {
+            printf("%d", dec[i] & a ? 1 : 0);
+            if(j == 4) printf(" ");
+            a >>= 1;
+        }
+        printf(" | ");
+    }
+    printf("\n");
+
+    return 0;
+}
