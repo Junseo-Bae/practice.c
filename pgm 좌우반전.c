ec450b92260882ed5ff955a021743b2330cd84c2
Junseo-Bae <74636948+Junseo-Bae@users.noreply.github.com>
2025-08-09 10:28:44 +0900

    Create pgm 좌우반전.c

---
+++ pgm 좌우반전.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+
+    char type[3] = "P5";
+    int width = 640;
+    int height = 426;
+    int color = 255;
+
+    FILE* file = fopen("sample_pgm.pgm", "rb");
+
+    fscanf(file, "%s\n%d %d\n%d", type, &width, &height, &color);
+
+    char* temp = (char*)malloc(sizeof(char) * width);
+    
+    fread(temp, sizeof(char), width * height, file);
+    fclose(file);
+    
+    
+
+    return 0;
+}
